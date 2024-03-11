1fef51ceb5ccefa89a83db6863de47fa389de41f
Harold <haroldrex12331@gmail.com>
2024-03-11 17:47:13 +0800

    first commit

---
+++ pages/error/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Error = () => {
+  return <Main>Error</Main>;
+};
+
+export default Error;
+
+const Main = styled.div``;
---
+++ pages/home/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const HomePage = () => {
+  return <Main>HomePage</Main>;
+};
+
+export default HomePage;
+
+const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  align-items: center;
+`;
