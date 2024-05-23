60968ffc647a8a824405ca435ad1ac052f6560b5
Harold <haroldrex12331@gmail.com>
2024-05-23 09:49:50 +0800

    video sdk self not defined

--- pages/example/index.tsx
+++ pages/example/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useRef, useMemo, useEffect } from "react";
 import { useMeeting, useParticipant } from "@videosdk.live/react-sdk";
 import ReactPlayer from "react-player";
